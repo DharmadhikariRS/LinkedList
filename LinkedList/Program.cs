7ac68e20d882b4b327f1bfd7f60ef4a37c1a8bc8
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-08-24 17:14:01 +0530

    [Rushikesh]  Append node at last in  linked list (Append Method)

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -9,6 +9,7 @@
             LinkedList linkedList = new LinkedList();
             Console.WriteLine("Press 1 for create a linked List");
             Console.WriteLine("Press 2 for Add node at first in  linked List");
+            Console.WriteLine("Press 3 to append node at last in linked List");
             int input = int.Parse(Console.ReadLine());
             switch (input)
             {
@@ -24,6 +25,13 @@
                     linkedList.AddFirst(56);
                     linkedList.Display();
                     break;
+                    case 3:
+                    linkedList.AddLast(56);
+                    linkedList.AddLast(30);
+                    linkedList.AddLast(70);
+                    linkedList.Display();
+                    break;
+
                 default:
                     Console.WriteLine("Wrong selection");
                     break;
