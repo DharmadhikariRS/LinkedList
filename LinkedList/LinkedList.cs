832a2e16361395b7bc0e0ea43f660357fdec9d16
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-08-24 16:52:07 +0530

    [Rushikesh]  Create linked list program

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedList
+    {
+        internal Node Head = null;
+        internal Node Tail = null;
+
+        internal void AddLast(int Data)
+        {
+            Node node = new Node(Data);
+            if (Head == null)
+            {
+                Head = node;
+                //    Tail=Head;
+                Console.WriteLine("added " + Head.data);
+            }
+            else
+            {
+                Node temp = Head;
+                while (temp.Next != null)
+                {
+
+                    temp = temp.Next;
+
+                }
+
+                temp.Next = node;
+                Console.WriteLine("added " + temp.Next.data);
+                //     Tail = temp;
+            }
+        }
+        internal void Display()
+        {
+            Node temp = Head;
+            Console.WriteLine("List Elements are:");
+            while (temp != null)
+            {
+                Console.Write("{0} ", temp.data);
+                temp = temp.Next;
+
+            }
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List Programe");
+
+            LinkedList linkedList = new LinkedList();
+            Console.WriteLine("Press 1 for create a linked List");
+            int input = int.Parse(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    linkedList.AddLast(56);
+                    linkedList.AddLast(30);
+                    linkedList.AddLast(70);
+                    linkedList.Display();
+                    break;
+                default:
+                    Console.WriteLine("Wrong selection");
+                    break;
+
+            }
+        }
+    }
+}
